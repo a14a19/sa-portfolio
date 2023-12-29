b930194ff7d9ac1893ff67a7fd714d01687ffe4c
mzsellem <marleysellem@gmail.com>
2023-12-29 17:18:59 -0500

    card layout

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -5,12 +5,11 @@ import { education } from "../../data/education";
 import SmallCard from "./SmallCard";
 
 function Card(props) {
-  
    // console.log(props.type)
    return (
       <>
          {props.type === "EXPERIENCE" && (
-            <aside className="w-[33.333%] h-min pt-1 border border-[#d8d8d830] bg-[#1e1e1e] rounded-xl overflow-hidden">
+            <aside className="w-full h-min pt-1 border border-[#d8d8d830] bg-[#1e1e1e] rounded-xl overflow-hidden">
                <Link className="w-full px-3 text-xs">EXPERIENCE</Link>
                <div className="mx-2 mt-1 border-b-2 border-[#d8d8d830]"></div>
                <div className="h-[31.25rem] overflow-y-auto px-3">
@@ -35,22 +34,24 @@ function Card(props) {
             </aside>
          )}
          {props.type === "TIMEMODE" && (
-            <aside className="w-[33.333%] flex flex-col gap-5 h-min">
+            <aside className="flex flex-col w-full gap-5 h-min">
                <div className="flex gap-5">
-                    <SmallCard type={"TIME"} />
-                    <SmallCard type={"MODE"} />
-                </div>
-                <aside className="w-full h-min pt-1 border border-[#d8d8d830] bg-[#1e1e1e] rounded-xl overflow-hidden">
-                    <Link className="text-xs w-full px-3">WHAT I DO</Link>
-                    <div className="mx-2 border-b-2 mt-1 border-[#d8d8d830]" ></div>
-                    <div className="h-[19.43rem] overflow-y-auto px-3 pt-3 flex flex-col gap-3 relative">
-                        <button className="px-3 py-2 bg-[#fac99c] w-min whitespace-nowrap text-sm text-black rounded-lg pm-animate absolute top-10 left-8">Product Manager</button>
-                    </div>
-                </aside>
+                  <SmallCard type={"TIME"} />
+                  <SmallCard type={"MODE"} />
+               </div>
+               <aside className="w-full h-min pt-1 border border-[#d8d8d830] bg-[#1e1e1e] rounded-xl overflow-hidden">
+                  <Link className="w-full px-3 text-xs">WHAT I DO</Link>
+                  <div className="mx-2 border-b-2 mt-1 border-[#d8d8d830]"></div>
+                  <div className="h-[19.43rem] overflow-y-auto px-3 pt-3 flex flex-col gap-3 relative">
+                     <button className="px-3 py-2 bg-[#fac99c] w-min whitespace-nowrap text-sm text-black rounded-lg pm-animate absolute top-10 left-8">
+                        Product Manager
+                     </button>
+                  </div>
+               </aside>
             </aside>
          )}
          {props.type === "PROJECT" && (
-            <aside className="w-[33.333%] h-min pt-1 border border-[#d8d8d830] bg-[#1e1e1e] rounded-xl overflow-hidden">
+            <aside className="w-full h-min pt-1 border border-[#d8d8d830] bg-[#1e1e1e] rounded-xl overflow-hidden">
                <Link className="w-full px-3 text-xs">PROJECT</Link>
                <div className="mx-2 border-b-2 mt-1 border-[#d8d8d830]"></div>
                <div className="h-[20.625rem] overflow-y-auto px-3 pt-3 flex flex-col gap-3 ">
@@ -80,7 +81,7 @@ function Card(props) {
          )}
 
          {props.type === "KINDWORDS" && (
-            <aside className="w-[33.333%] h-min pt-1 border border-[#d8d8d830] bg-[#1e1e1e] rounded-xl overflow-hidden">
+            <aside className="w-full h-min pt-1 border border-[#d8d8d830] bg-[#1e1e1e] rounded-xl overflow-hidden">
                <Link className="w-full px-3 text-xs">KIND WORDS</Link>
                <div className="mx-2 border-b-2 mt-1 border-[#d8d8d830]"></div>
                <div className="h-[20.625rem] overflow-y-auto px-3 pt-3 flex flex-col gap-3 text-xs">
@@ -94,16 +95,16 @@ function Card(props) {
             </aside>
          )}
          {props.type === "YEARSPROCTAS" && (
-            <aside className="w-[33.333%] flex gap-5 h-min">
+            <aside className="flex w-full gap-5 h-[22.8rem]">
                <SmallCard type={"YEARSPRO"} />
                <SmallCard type={"CTAS"} />
             </aside>
          )}
          {props.type === "EDUCATION" && (
-            <aside className="w-[33.333%] h-min pt-1 border border-[#d8d8d830] bg-[#1e1e1e] rounded-xl overflow-hidden">
+            <aside className="w-full h-min pt-1 border border-[#d8d8d830] bg-[#1e1e1e] rounded-xl overflow-hidden">
                <Link className="w-full px-3 text-xs">EDUCATION</Link>
                <div className="mx-2 border-b-2 mt-1 border-[#d8d8d830]"></div>
-               <div className="h-[20.625rem] overflow-y-auto px-3 pt-3 flex flex-col gap-3 ">
+               <div className="h-[31.3rem] overflow-y-auto px-3 pt-3 flex flex-col gap-3 ">
                   {education &&
                      education.map((elem, i) => {
                         return (
