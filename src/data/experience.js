0d26a6080847d04467b9bbc7d6737bde31b6ce5b
a14a19 <anas31197@gmail.com>
2023-12-30 02:35:42 +0530

    what i do completed, exp & start up content changed

--- src/data/experience.js
+++ src/data/experience.js
@@ -1,32 +1,32 @@
 export const experience = [
     {
         title: "Founder // Stealth Startup",
-        description: "A stealth startup operates in secrecy to avoid attention, protecting details from competitors. Investors may disclose funding, but limited information is shared to guard against idea theft.",
+        description: "Leading covert initiatives, navigating uncertainties, and ensuring the success of the startup through strategic planning and execution in a discreet environment.",
         location: "- Worldwide"
     },
     {
         title: "General Partner // Loading Fund",
-        description: "Unleashing Potential, One Investment at a Time. Powering the Future of Innovation with Purposeful Capital.",
+        description: "Driving investment strategies, cultivating relationships, and providing strategic leadership to accelerate the growth and success of portfolio companies.",
         location: "- NY & LND"
     },
     {
         title: "Partner // SLA",
-        description: "Sridhar Legal Associates is a global law firm. The firm has internationally recognized practices in a wide spectrum of transactional, litigation, corporate, VC/PE and regulatory areas.",
+        description: "Delivering expert legal counsel, overseeing client relations, and guiding strategic initiatives to ensure effective legal representation and client success.",
         location: "- New York"
     },
     {
         title: "Entrepreneur in Residence // VC",
-        description: "A stealth startup operates in secrecy to avoid attention, protecting details from competitors. Investors may disclose funding, but limited information is shared to guard against idea theft.",
+        description: "Catalyzing innovation, evaluating investments, and mentoring startups for strategic growth within the venture capital landscape.",
         location: "- Global"
     },
     {
         title: "Founder // iUXD Studio",
-        description: "iUXD Studio is a design collective specialising in user experience, brand & identity, and motion graphics. helped many businesses solve their complex Product UX challenges and make the experience joyful for their users.",
+        description: "Shaping creative vision, spearheading innovative design, and enhancing user experiences to redefine brand interactions.",
         location: "- Chennai, India"
     },
     {
         title: "Software Engineer & Product Manager // Various Startup",
-        description: "",
+        description: "Merging tech prowess with strategic vision for seamless product development and innovative solutions.",
         location: "- Chennai, India"
     },
 ]
